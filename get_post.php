2183f17b5a92f4b40c1cac68509e8dd1e61a66b7
liuqun <liuqun0319@163.com>
2018-06-25 18:03:00 +0800

    get和post封装

---
+++ get_post.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * GET 请求
+ * @param string $url
+ */
+function http_get($url) {
+    $oCurl = curl_init();
+    if (stripos($url, "https://") !== FALSE) {
+        curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, FALSE);
+        curl_setopt($oCurl, CURLOPT_SSLVERSION, 1);
+    }
+    curl_setopt($oCurl, CURLOPT_URL, $url);
+    curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($oCurl, CURLOPT_USERAGENT, 'User-Agent:Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36');
+    curl_setopt($oCurl, CURLOPT_TIMEOUT, 60);
+    $sContent = curl_exec($oCurl);
+    $aStatus = curl_getinfo($oCurl);
+    curl_close($oCurl);
+    if (intval($aStatus["http_code"]) == 200) {
+        return $sContent;
+    } else {
+        return false;
+    }
+}
+
+/**
+ * POST 请求
+ * @param string $url
+ * @param array $param
+ * @param boolean $post_file 是否文件上传
+ * @return string content
+ */
+function http_post($url, $param, $post_file = false) {
+    $oCurl = curl_init();
+    if (stripos($url, "https://") !== FALSE) {
+        curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($oCurl, CURLOPT_SSLVERSION, 1);
+    }
+    if (is_string($param) || $post_file) {
+        $strPOST = $param;
+    } else {
+        $aPOST = array();
+        foreach ($param as $key => $val) {
+            $aPOST[] = $key . "=" . urlencode($val);
+        }
+        $strPOST = join("&", $aPOST);
+    }
+    curl_setopt($oCurl, CURLOPT_URL, $url);
+    curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($oCurl, CURLOPT_TIMEOUT, 120);
+    curl_setopt($oCurl, CURLOPT_POST, true);
+    curl_setopt($oCurl, CURLOPT_POSTFIELDS, $strPOST);
+    $sContent = curl_exec($oCurl);
+    $aStatus = curl_getinfo($oCurl);
+    curl_close($oCurl);
+    if (intval($aStatus["http_code"]) == 200) {
+        return $sContent;
+    } else {
+        return false;
+    }
+}
